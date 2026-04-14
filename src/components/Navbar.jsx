01974be7c81c0d8bd0d337d8829db622af671855
“kd <kd.bromley1@gmail.com>
2026-04-14 12:27:19 -0700

    make all links lowercase in index.css; populate Navbar.jsx, create Navlinks/prepare router; create Navbar.css and basic styling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,38 @@
+import { NavLink, Link } from "react-router-dom"
+import "./Navbar.css"
+
 function Navbar() {
-  return <div>Navbar</div>
+  return (
+    <nav className="navbar" id="navbar">
+     <div className="navbar__brand">
+       <h3>k.d. Bromley</h3>
+     </div>
+
+     <div className="navbar__links">
+      <NavLink
+        to="/#work"
+        id="navbar__link-work"
+        className={({ isActive }) =>
+          isActive ? "active" : ""
+        }
+      > Work </NavLink>
+      <NavLink
+        to="/#wrangling"
+        id="navbar__link-wrangling"
+        className={({ isActive }) =>
+          isActive ? "active" : ""
+        }
+      > Wrangling </NavLink>
+      <NavLink
+        to="/#writing"
+        id="navbar__link-writing"
+        className={({ isActive }) =>
+          isActive ? "active" : ""
+        }
+      > Writing </NavLink>
+      </div>
+    </nav>
+  )
 }
 
 export default Navbar
