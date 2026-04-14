e0f5fdb8ea097dd866469aab9104d22a985f08b4
“kd <kd.bromley1@gmail.com>
2026-04-13 22:46:26 -0700

    move ProjectDetail to pages folder; create router in App.jsx, render placeholders for basic structure (Navbar, ScrollPage, and Footer)

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import  ScrollPage  from './pages/ScrollPage'
+import  ProjectDetail  from './pages/ProjectDetail'
+import  Navbar  from './components/Navbar'
+import Footer from './components/Footer'
+
 import './App.css'
 
 function App() {
- return (
- <>
- Hello, World!
- </>
- )
+    return (
+        <BrowserRouter>
+            <Navbar />
+
+            <Routes>
+                <Route path="/" element={<ScrollPage />} />
+                <Route path="/work/:slug" element={<ProjectDetail />} />
+            </Routes>
+
+            <Footer />
+        </BrowserRouter>
+    )
 }
 
 export default App
