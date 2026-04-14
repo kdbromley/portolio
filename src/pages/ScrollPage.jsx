a9170f9071fd95f30f461aa260abc3b6def2b366
“kd <kd.bromley1@gmail.com>
2026-04-13 22:58:16 -0700

    add section components to ScrollPage

---
+++ src/pages/ScrollPage.jsx
@@ -0,0 +1,17 @@
+import Hero from '../components/Hero';
+import WebDev from '../components/WebDev';
+import Wrangling from '../components/Wrangling';
+import Writing from '../components/Writing';
+
+function ScrollPage() {
+  return (
+    <div>
+      <Hero />
+      <WebDev />
+      <Wrangling />
+      <Writing />
+    </div>
+  )
+}
+
+export default ScrollPage
