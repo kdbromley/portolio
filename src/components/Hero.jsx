f7af5a750c55bc2b74834e71ba22916e366191c4
“kd <kd.bromley1@gmail.com>
2026-04-14 11:58:27 -0700

    populate Hero section, create Hero style page and add basic styling, update index.css to default headings to sans font

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,14 @@
+import './Hero.css'
+
 function Hero() {
-  return <div>Hero</div>
+  return (
+    <section className="hero" id="hero">
+      <h1>k.d. Bromley</h1>
+      <h2>Web Developer</h2>
+      <p className='bio'>I build things for the web and write about the strange corners of the American West. I spent a couple of years away from the tech industry chasing a lifelong dream of working on a ranch as a wrangler — now I'm back, looking to build my career.</p>
+      <a className='cta' href='#webdev'>see my work → </a> <a className='cta'href='mailto:kd.bromley1@gmail.com'>get in touch → </a>
+    </section>
+  )
 }
 
 export default Hero
