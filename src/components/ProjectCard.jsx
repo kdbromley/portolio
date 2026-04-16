9286fcce6ec0291bb8fc1b47b6c8e95c8d1ab977
“kd <kd.bromley1@gmail.com>
2026-04-16 11:12:07 -0700

    update classnames, css improvements and add size vars for font in root, populate ProjectCard

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,7 +1,17 @@
 import "./ProjectCard.css"
 
 function ProjectCard({ project }) {
-  return <div className="projectcard__container">ProjectCard for {project.title}</div>
+  return ( 
+    <div className="projectcard">
+      <h4 className="projectcard__title">{project.title}</h4>
+      <h5 className="projectcard__employer">{project.employer}</h5>
+      {project.link 
+        ? <a className="projectcard__link" href={project.link}>{project.linkLabel}</a>
+        : <span className="projectcard__link">{project.linkLabel}</span>
+      }
+    </div>
+
+  )
 }
 
 export default ProjectCard
--- src/components/WebDev.jsx
+++ src/components/WebDev.jsx
@@ -5,9 +5,13 @@ import "./WebDev.css"
 function WebDev() {
   return (
     <section className="webdev" id="work">
-      <h3>Web Development</h3>    
+      <h3 className="webdev__header">Web Development</h3>    
+      <p className="webdev__intro">
+        My primary work experience comes from my time as a Junior Web Developer at Plucky, from 2021-2023, primarily front-end development in Wordpress and development and database management in Claris Filemaker. 
+        During my time working as a wrangler, I picked up a few freelance gigs, largely in Shopify. 
+      </p>
       <div className="webdev__projects">
-        <h4>Selected Projects</h4>
+        <h4 className="webdev__projects-header">Selected Projects</h4>
         <div className="webdev__projects-list">
         {projects
         .filter((project) => project.featured === true)
